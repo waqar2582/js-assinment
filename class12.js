cabbbdf9c8eb26c8aa55cead2ab3f6f04a4c3f8e
user-name <waqaryounus2582@gmail.com>
2023-09-01 18:13:16 +0500

    first commit

---
+++ class12.js
@@ -0,0 +1,68 @@
+// Question 1
+ 
+// alert("Hello Welcome to Javascript world")
+
+// Question 2
+// alert("Erorr! Please enter a valid password")
+
+// Question 3
+
+// alert("Welcome to JS Land...\n Happy Coding! ")
+
+// Question 4
+
+// alert("Welcome to JS Land... ")
+// alert("Happy Coding! \n▢prevent this page  from creating additional dialog ")
+ 
+// Question 5
+
+// let text ;
+// let text1 = prompt("Please enter your message")
+// alert(text1)
+
+
+
+// chapter 2
+
+// Question 1 and 2 
+
+// var username;
+// var myName = "Muhammmad Umer Hayat";
+// alert(myName)
+
+// Question 3
+
+// var message = "Hello World!";
+// alert(message)
+
+
+// Question 4
+// var studentData = "My Name is Muhammd Umar Hayat \n I'm 30 year old \n and I am certified Web and Mobile Application Developer\n";
+// alert(studentData)
+
+// Question 5
+ 
+// var pizza = "Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+
+// Question 6
+
+// var email = "My Email address is waqar@gmail.com ";
+// alert(email)
+
+// Question 7
+
+// var learn = "I'm tryning to Learn from the  Book “A smarter way to learn JavaScript”.";
+// alert(learn);
+
+
+// Question 7
+
+// var content = "yeah! I can write HTML content through JavaScript ";
+// document.write(content);
+
+
+// Question 8
+
+// var ajeebSatext = "“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(ajeebSatext);
